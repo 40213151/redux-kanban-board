96e42d72aa0bd424931d8154e15e6c350dae8df1
daimaru.keiichiro <daimaru.keiichiro@DMPC-049.local>
2023-06-28 02:41:15 +0900

    AIP通信で投稿内容を永続化する

---
+++ src/api.ts
@@ -0,0 +1,86 @@
+export type ReqAndRes = {
+    'POST /v1/cards': {
+      req: {
+        id: string
+        text?: string
+      }
+      res: {
+        id: string
+        text?: string
+      }
+    }
+}
+  
+  export const Endpoint = 'http://localhost:3000/api'
+  
+  export async function api<K extends keyof ReqAndRes>(
+    key: K,
+    payload: ReqAndRes[K]['req'],
+
+    // めも
+    // async関数はデフォルトでPromiseを返すけど
+    // TypeScriptでは関数の戻り値の型を明示的に指定することで、型安全性を向上させることができるので、この書き方をしている。
+    // ReqAndRes[K]['res']では、レスポンスの型を指定している。
+  ): Promise<ReqAndRes[K]['res']> {
+    const [method, path] = key.split(' ')
+    if (!method || !path) {
+      throw new Error(`Unrecognized api: ${key}`)
+    }
+  
+    let pathWithID = ''
+    const option: RequestInit = { method }
+    switch (option.method) {
+      case 'GET':
+      case 'DELETE':
+        if (payload && 'id' in payload) {
+          pathWithID = `${path}/${payload.id}`
+        }
+        break
+  
+      case 'POST':
+        option.headers = { 'Content-Type': 'application/json' }
+        option.body = JSON.stringify(payload)
+        break
+  
+      case 'PATCH':
+        if (payload && 'id' in payload) {
+          pathWithID = `${path}/${payload.id}`
+        }
+        option.headers = { 'Content-Type': 'application/json' }
+        option.body = JSON.stringify(payload)
+        break
+    }
+  
+    return fetch(`${Endpoint}${pathWithID || path}`, option).then(res =>
+      res.ok
+        ? res.json()
+        : res.text().then(text => {
+            throw new APIError(
+              method,
+              res.url,
+              res.status,
+              res.statusText,
+              res.ok,
+              res.redirected,
+              res.type,
+              text,
+            )
+          }),
+    )
+  }
+  
+  export class APIError extends Error {
+    constructor(
+      public method: string,
+      public url: string,
+      public status: number,
+      public statusText: string,
+      public ok: boolean,
+      public redirected: boolean,
+      public type: string,
+      public text?: string,
+    ) {
+      super(`${method} ${url} ${status} (${statusText})`)
+    }
+  }
+  
